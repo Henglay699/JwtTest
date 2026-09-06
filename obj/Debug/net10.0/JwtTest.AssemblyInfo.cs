fdccb2d187b092a36bdcdf4ef9b2b90c180ab492
henglay <henglay@example.com>
2026-09-06 18:06:01 +0700

    Correct Version

--- obj/Debug/net10.0/JwtTest.AssemblyInfo.cs
+++ obj/Debug/net10.0/JwtTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a090c7665c6772f175241e7df737511f801534aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20b7cfdfef41986f906ff6401e9b2e3c6ff4d2d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
